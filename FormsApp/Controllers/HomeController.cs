796223d1bc0b561bd4f2a2a9832219708c1e649e
MERVE EKŞİ <merveeksii61@gmail.com>
2024-07-30 01:36:53 +0300

    Categories select

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -11,14 +11,20 @@ public class HomeController : Controller
     {
     }
 
-    public IActionResult Index(string searchString)
+    public IActionResult Index(string searchString, string category)
     {
         var Products = Repository.Products;
-        if (!string.IsNullOrEmpty(searchString))
+        if (!String.IsNullOrEmpty(searchString))
         {
             ViewBag.SearchString = searchString;
             products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
         }
+        if (!String.InNullOrEmpty(category) && category !="0")
+        {
+            products = products.Where(p => p.CategoryId ==int.Parse(category)).ToList();
+        }
+
+        ViewBag.Categories = now SelectList(Repository.Categories, "CategoryId", "Name")
         return View(products);
     }
 
