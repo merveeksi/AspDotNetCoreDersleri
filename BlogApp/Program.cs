087e6e50623aac3e5934668db1d4716944fa4381
MERVE EKŞİ <merveeksii61@gmail.com>
2024-08-12 19:15:37 +0300

    Connection String

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,5 +1,15 @@
+using BlogApp.Data.Concrete.EfCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddDbContext<BlogContext>(options =>
+{
+    var config = builder.Configuration; 
+    var connectionString = config.GetConnectionString("sql_connection"); 
+    options.UseSqlite(); 
+});
 
 app.MapGet("/", () => "Hello World!");
 
