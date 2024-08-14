aac08426b156ef3043ae062efa9656d566f1c8bd
MERVE EKŞİ <merveeksii61@gmail.com>
2024-08-14 09:15:09 +0300

    mysql database provider

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -7,9 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BlogContext>(options =>
 {
     var config = builder.Configuration; 
-    var connectionString = config.GetConnectionString("sql_connection"); 
-    options.UseSqlite(connectionString); 
+    var connectionString = config.GetConnectionString("mysql_connection"); 
+    //options.UseSqlite(connectionString); 
+    
+    var version = new MySqlServerVersion(new Version(9, 0, 1));
+    options.UseMySql(connectionString, version);
 });
+
 
 var app = builder.Build();
 
